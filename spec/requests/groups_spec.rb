f9e471fbee0a100d066ed9984dca0ac88e9d728e
Monica Wilkinson <monica.keller@gmail.com>
2011-08-06 19:52:39 -0700

    find the nested resource paths later

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "Groups" do
-  describe "GET /groups" do
-    it "works! (now write some real specs)" do
+  describe "GET /orgs/:id/groups" do
+    it "works (now write some real specs)" do
+      @org = Organization.create! :display_name => 'foo'
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get groups_path
-      response.status.should be(200)
+      #get groups_organization_path @org
+      #response.status.should be(200)
+      pending "find the path"
     end
   end
 end
--- spec/requests/owned_resources_spec.rb
+++ spec/requests/owned_resources_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "OwnedResources" do
-  describe "GET /owned_resources" do
+  describe "GET /orgs/1/owned_resources" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get owned_resources_path
-      response.status.should be(200)
+      #get owned_resources_path
+      #response.status.should be(200)
+      pending "find the path"
     end
   end
 end
