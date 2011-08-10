e9bf322112f9984caa582b9482f8af2b708a2c7a
Monica Wilkinson <monica.keller@gmail.com>
2011-08-10 14:41:08 -0700

    Fixed default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,13 @@ pwd = 'cloud$'
 @user = User.create! :first_name => 'Dale', :last_name => 'Olds', :display_name => 'Dale O.', :password => pwd, :confirm_password => pwd, :email => 'olds@vmware.com'
 @user2 = User.create! :first_name => 'Monica', :last_name => 'Wilkinson', :display_name => 'Monica W.', :password => pwd, :confirm_password => pwd, :email => 'mwilkinson@vmware.com'
 
-@org = Org.create! :display_name => 'VMWare'
-@group = @org.groups.build :display_name => 'All employees'
-@group.save!
+@org = Org.create! :display_name => 'VMWare', :creator => @user
+
+@group = @org.groups.find_by_display_name 'Admins'
 @group_member = @group.group_members.build :user => @user
 @group_member.save!
 @group_member2 = @group.group_members.build :user => @user2
 @group_member2.save!
 
-@service = Service.create! :display_name => 'Identity Service', :creator => @user
-@app = App.create! :display_name => 'Collaborate', :creator => @user2
-
-@project = @org.projects.build :display_name => 'Default'
-@project.save!
-@owned_resource = @org.owned_resources.build :resource => @app
-@owned_resource.save!
-@owned_resource2 = @org.owned_resources.build :resource => @service
-@owned_resource2.save!
-
-@acl = @project.acls.build :owned_resource => @owned_resource2, :entity => @user, :read_bit => true, :update_bit => true
-@acl.save!
-
-@acl2 = @project.acls.build :route => "owned_resource/2/resource", :entity => @user2, :read_bit => true, :update_bit => true
-@acl2.save!
+@service = Service.create! :display_name => 'Identity Service', :creator => @user, :project => @org.default_project
+@app = App.create! :display_name => 'Collaborate', :creator => @user2, :project => @org.default_project
