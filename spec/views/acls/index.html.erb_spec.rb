bf16c9e72696fee2674c73dbba3dcb936c3035be
Monica Wilkinson <monica.keller@gmail.com>
2011-09-13 11:48:21 -0700

    Fix Acl index view tests

--- spec/views/acls/index.html.erb_spec.rb
+++ spec/views/acls/index.html.erb_spec.rb
@@ -3,24 +3,19 @@ require 'spec_helper'
 describe "acls/index.html.erb" do
   before(:each) do
     @org = assign(:org, stub_model(Org, :display_name => 'VMWare'))
-    @project = assign(:project, stub_model(Project, :org => @org, :display_name => 'Cloud Foundry'))
-    @app = assign(:app, stub_model(App, :display_name => 'Optimus', :project => @project))
-    @owned_resource = assign(:owned_resource, stub_model(OwnedResource, :org => @org, :resource => @app))
-    @user = assign(:user, stub_model(User, :first_name => 'Monica', :last_name => 'Wilkinson', :display_name => 'Monica W'))
-    @acl = assign(:acl, stub_model(Acl, :owned_resource => @owned_resource, :entity => @user))
+    @project = stub_model(Project, :org => @org, :display_name => 'Cloud Foundry')
+    @user = stub_model(User, :first_name => 'Monica', :last_name => 'Wilkinson', :display_name => 'Monica W')
+    @user2 = stub_model(User, :first_name => 'Peter', :last_name => 'Jenkins', :display_name => 'Jenkins')
+    @acl = stub_model(Acl, :route => "/groups/*", :entity => @user, :permission_set => PermissionManager::ALL, :project => @project)
+    @acl2 = stub_model(Acl, :route => "/apps/*", :entity => @user, :permission_set => PermissionManager::ALL, :project => @project)
+    @acl3 = stub_model(Acl, :route => "/apps/*", :entity => @user2, :permission_set => PermissionManager::ALL, :project => @project)
+    assign(:acls, [@acl, @acl2, @acl3])
   end
 
   it "renders a list of acls" do
     render
-    ## Run the generator again with the --webrat flag if you want to use webrat matchers
-    #assert_select "tr>td", :text => 1.to_s, :count => 2
-    ## Run the generator again with the --webrat flag if you want to use webrat matchers
-    #assert_select "tr>td", :text => 1.to_s, :count => 2
-    ## Run the generator again with the --webrat flag if you want to use webrat matchers
-    #assert_select "tr>td", :text => 1.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Entity Type".to_s, :count => 2
-    ## Run the generator again with the --webrat flag if you want to use webrat matchers
-    #assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => @user.display_name, :count => 2
+    assert_select "tr>td", :text => "User", :count => 3
+    assert_select "tr>td", :text => "/apps/*", :count => 2
   end
 end
