8b6218bb7c1ca5025664b41c1631cd4fae5066b7
Monica Wilkinson <monica.keller@gmail.com>
2011-09-26 15:31:50 -0700

    Modified to handle oauth callbacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,23 +26,25 @@ class ApplicationController < ActionController::Base
   end
 
   def check_action_can_be_done
-    permission_needed = REST_TO_CRUD[params[:action].to_sym]
-    path = request.path
-    org = nil
-    logger.info "Starting Authorization for #{current_user}"
-    if (request.path_parameters.has_key? :org_id)
-      org = Org.find params[:org_id]
-      logger.info "Org = #{org}"
-      path.gsub! "/orgs/#{params[:org_id]}/", ''
-      if (org && !permission_needed.nil? & path)
-        org.projects.each do |project|
-          return true if project.can_user permission_needed, path, current_user
-          logger.info " No permission #{permission_needed} in project #{project} for path #{path}"
+    if params[:action]
+      permission_needed = REST_TO_CRUD[params[:action].to_sym]
+      path = request.path
+      org = nil
+      logger.info "Starting Authorization for #{current_user}"
+      if (request.path_parameters.has_key? :org_id)
+        org = Org.find params[:org_id]
+        logger.info "Org = #{org}"
+        path.gsub! "/orgs/#{params[:org_id]}/", ''
+        if (org && !permission_needed.nil? & path)
+          org.projects.each do |project|
+            return true if project.can_user permission_needed, path, current_user
+            logger.info " No permission #{permission_needed} in project #{project} for path #{path}"
+          end
+          redirect_to :root, :status => 401
+          return false
         end
-        redirect_to :root, :status => 401
-        return false
       end
-      end
+    end
     logger.info "No security controls for #{path}"
     return true
 
