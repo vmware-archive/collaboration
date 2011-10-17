019682d96d2647e3772b36f34eafb65edbc97b7a
Monica Wilkinson <monica.keller@gmail.com>
2011-10-17 16:21:35 -0700

    Test Gallore

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -137,4 +137,88 @@ describe Users::OmniauthCallbacksController do
       end
     end
   end
+
+  context "with existing user in db and different user signed in" do
+    before(:each) do
+      pwd = 'cloud$'
+      @user = User.create! :first_name => 'Test', :last_name => 'Testing', :display_name => 'Tester', :password => pwd, :confirm_password => pwd, :email => @email
+      @user2 = User.create! :first_name => 'Bla', :last_name => 'Blah', :display_name => 'Blaher', :password => pwd, :confirm_password => pwd, :email => "Blablah@vmware.com"
+      sign_in @user2
+    end
+
+    describe "GET endpoint with matching data" do
+
+      PROVIDERS.each do |provider|
+        before(:each) do
+          env = {"omniauth.auth" => @hash[provider]}
+          @controller.stub!(:env).and_return(env)
+          get provider
+        end
+
+        it "should find the user by email" do
+          assert_redirected_to root_path
+          assert_match flash[:notice], /Successfully authorized from .+ account/
+        end
+
+        it { should be_user_signed_in }
+
+        it "should have the user matching oauth user signed in" do
+          subject.current_user.should == @user
+        end
+
+        it "should store the access token for the provider" do
+          UserAccessToken.get_access_tokens(@user, provider).count.should == 1
+        end
+      end
+    end
+  end
+
+  context "with no matching user in db and a user signed in" do
+    before(:each) do
+      pwd = 'cloud$'
+      @user = User.create! :first_name => 'Bla', :last_name => 'Blah', :display_name => 'Blaher', :password => pwd, :confirm_password => pwd, :email => "Blablah@vmware.com"
+      sign_in @user
+    end
+
+    describe "GET endpoint with proper data" do
+      PROVIDERS.each do |provider|
+        before(:each) do
+          env = {"omniauth.auth" => @hash[provider]}
+          @controller.stub!(:env).and_return(env)
+          get provider
+        end
+
+        it { should be_user_signed_in }
+
+        it "should have the user matching oauth user signed in" do
+          subject.current_user.should == @user
+        end
+
+        it "should store the access token for the provider" do
+          UserAccessToken.get_access_tokens(@user, provider).count.should == 1
+        end
+
+        it "should add an alternate email address to the user" do
+          @user.reload.email_addresses.collect(&:email).should == [@user.email, @email]
+        end
+
+        describe "user signs out" do
+          before(:each) do
+            sign_out @user
+            get provider
+          end
+
+          it { should be_user_signed_in }
+
+          it "should have the user matching oauth user signed in" do
+            subject.current_user.should == @user
+          end
+
+          it "should store the access token for the provider" do
+            UserAccessToken.get_access_tokens(@user, provider).count.should == 1
+          end
+        end
+      end
+    end
+  end
 end
